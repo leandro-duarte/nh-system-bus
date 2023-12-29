4a9a0b90a99e4f6ac8e802c5cdb64f42c04de713
Leandro Duarte <leandroduarte.it@gmail.com>
2023-12-29 13:48:28 -0300

    feat:update schemas

--- backend/src/services/fieldValidations/busLineSchema.ts
+++ backend/src/services/fieldValidations/busLineSchema.ts
@@ -5,7 +5,7 @@ export const busLineSchema = Joi.object({
     code:Joi.number().integer().required().min(3).max(3),
     name: Joi.string().max(50).required(),
     timeStart:Joi.string().required().min(5) .max(5),
-    timeEnd:Joi.number().integer().required(),
+    timeEnd:Joi.string().required().min(5).max(5),
     ridership:Joi.number().integer().required().max(12),
     vehicleId:Joi.number().integer().required(),
     companyId:Joi.number().integer().required()
